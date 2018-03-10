c423fa23f57b40d5ac760a9d6b6221e7af9f844e
MD Shahed Alam <Shahedalam@users.noreply.github.com>
2018-03-10 16:04:01 +0600

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
-sudo apt-get update
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo apt-get update -y
 sudo update-alternatives --config java
